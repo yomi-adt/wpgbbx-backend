2753f8fefd57d0c2b621f6b05e514de23a7267b6
yomi-adt <nbarlaab@gmail.com>
2026-07-13 19:08:53 -0500

    CORS

---
+++ src/main/java/yomi_adt/wpgbbx/config/WebConfig.java
@@ -0,0 +1,26 @@
+package yomi_adt.wpgbbx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://yomi-adt.github.io/beyblade_ranking/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
