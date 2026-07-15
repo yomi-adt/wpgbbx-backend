8e63ed8df2bae3fc14ec9d7c5417e6f2bd621c35
yomi-adt <nbarlaab@gmail.com>
2026-07-15 13:36:39 -0500

    added debug endpoint

--- src/main/java/yomi_adt/wpgbbx/controller/BaseController.java
+++ src/main/java/yomi_adt/wpgbbx/controller/BaseController.java
@@ -1,14 +1,23 @@
 package yomi_adt.wpgbbx.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class BaseController {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @GetMapping("/ping")
     public String ping() {
         System.out.println("Got a ping request");
         return "Backend is reachable!";
+    }
+
+    @GetMapping("/debug")
+    public String debug() {
+        return mongoTemplate.getDb().getName();
     }
 }
